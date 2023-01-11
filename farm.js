386638adbf47c6dc0fb83f943986f1df9643d736
Angelia Beek <angeliabeek@outlook.com>
2023-01-11 17:45:07 +0100

    Added functionalities

--- farm.js
+++ farm.js
@@ -8,16 +8,48 @@ const getYieldForCrop = (input) => {
 
 const getTotalYield = (input) => {
     const crops = input.crops;
-    const cropYield = crops.map(veggie => veggie.crop.yield * veggie.numCrops);
-    const totalYield = cropYield.reduce((accumulator, currentValue) => {
+    const cropYieldArray = crops.map(veggie => veggie.crop.yield * veggie.numCrops);
+    const totalYield = cropYieldArray.reduce((accumulator, currentValue) => {
         return accumulator + currentValue;
     });
     return totalYield;
 };
+
+const getCostsForCrop = (input) => {
+    return input.numCrops * input.crop.cost;
+};
+
+const getRevenueForCrop = (input) => {
+    const totalYield = input.crop.yield * input.numCrops;
+    return totalYield * input.crop.salePrice;
+};
+
+const getProfitForCrop = (input) => {
+    const revenue = getRevenueForCrop(input);
+    const costs = getCostsForCrop(input);
+    return revenue - costs;
+};
+
+const getTotalProfit = (input) => {
+    const crops = input.crops;
+    //console.log(crops);
+    const cropProfitArray = crops.map(veggie => {
+        return getRevenueForCrop(veggie) - getCostsForCrop(veggie);
+    });
+    const totalProfit = cropProfitArray.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    });
+    return totalProfit;
+};
+
 
 
 module.exports = {
     getYieldForPlant,
     getYieldForCrop,
     getTotalYield,
+    getCostsForCrop,
+    getRevenueForCrop,
+    getProfitForCrop,
+    getTotalProfit,
 }
--- farm.test.js
+++ farm.test.js
@@ -1,4 +1,11 @@
-const { getYieldForPlant, getYieldForCrop, getTotalYield } = require("./farm");
+const {
+    getYieldForPlant,
+    getYieldForCrop,
+    getTotalYield,
+    getCostsForCrop,
+    getRevenueForCrop,
+    getProfitForCrop,
+    getTotalProfit } = require("./farm");
 
 describe("getYieldForPlant", () => {
     const corn = {
@@ -49,5 +56,72 @@ describe("getTotalYield", () => {
         };
         const crops = [{ crop: corn, numCrops: 0 }];
         expect(getTotalYield({ crops })).toBe(0);
+    });
+});
+
+describe("getCostsForCrop", () => {
+    test("Calculate the cost for a crop", () => {
+        const maize = {
+            name: "maize",
+            cost: 1,
+        };
+        const input = {
+            crop: maize,
+            numCrops: 10,
+        };
+        expect(getCostsForCrop(input)).toBe(10);
+    });
+});
+
+describe("getRevenueForCrop", () => {
+    test("Calculate the revenue for a crop", () => {
+        const apple = {
+            name: "apple",
+            yield: 5,
+            salePrice: 2,
+        };
+        const input = {
+            crop: apple,
+            numCrops: 3,
+        };
+        expect(getRevenueForCrop(input)).toBe(30);
+    });
+});
+
+describe("getProfitForCrop", () => {
+    test("calculate the profit for a crop", () => {
+        const corn = {
+            name: "corn",
+            cost: 1,
+            yield: 3,
+            salePrice: 3,
+        };
+        const input = {
+            crop: corn,
+            numCrops: 10,
+        };
+        expect(getProfitForCrop(input)).toBe(80);
+    });
+});
+
+describe("getTotalProfit", () => {
+    test("Calculate the profit for multible crops", () => {
+        const corn = {
+            name: "corn",
+            cost: 1,
+            yield: 3,
+            salePrice: 3,
+        };
+        const pumpkin = {
+            name: "pumpkin",
+            cost: 2,
+            yield: 4,
+            salePrice: 5,
+        };
+        const crops = [
+            { crop: corn, numCrops: 10 },
+            { crop: pumpkin, numCrops: 2 },
+        ];
+        expect(getTotalProfit({ crops })).toBe(116);
     });
 });
